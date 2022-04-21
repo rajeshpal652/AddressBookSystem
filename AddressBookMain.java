dd1ec863cb2734094d1d5b6191ab8be771095e9b
rajeshpal652 <rajesh.pal.652@.com>
2022-04-21 19:29:23 +0530

    Added: AddressBookSystem : Adding a new contact to address book

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,11 +1,33 @@
 package com.bl.addressbook;
+
+import java.util.Scanner;
+
 /*
- * Author : Rajesh
- * Displaying welcome massage in the Address Book System.
+ * @Author : Rajesh
+ * Adding new contact to addressbook taking user input from console.
  */
 public class AddressBookMain {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Program!!");
+		
+		Scanner sc = new Scanner(System.in);
+		Address address = new Address();
+		System.out.println("Enter first name: ");
+		address.setFirstName(sc.next());
+		System.out.println("Enter last name: ");
+		address.setLastName(sc.next());
+		System.out.println("Enter email: ");
+		address.setEmail(sc.next());
+		System.out.println("Enter phone number: ");
+		address.setPhoneNumber(sc.nextLong());
+		System.out.println("Enter city: ");
+		address.setCity(sc.next());
+		System.out.println("Enter state: ");
+		address.setState(sc.next());
+		System.out.println("Enter zip code : ");
+		address.setZip(sc.nextLong());
+		
+		System.out.println(address.toString());
 	}
 }
