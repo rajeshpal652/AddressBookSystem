0739aec5b0e7d7123c999409972840fcf0280bdb
rajeshpal652 <rajesh.pal.652@.com>
2022-04-21 19:17:59 +0530

    Added: Address Book System creating contacts

---
+++ Address.java
@@ -0,0 +1,9 @@
+package com.bl.addressbook;
+/*
+ * Author: Rajesh
+ * Initializing variables of contacts.
+ */
+public class Address {
+	String firstName, lastName, email, city, state;
+	long phoneNumber, zip;
+}
