6dd8926a7d66a243c97355f674bd70b0681866eb
rajeshpal652 <rajesh.pal.652@.com>
2022-04-21 18:46:36 +0530

    Added: AddressBookSystem

---
+++ AddressBookMain.java
@@ -0,0 +1,11 @@
+package com.bl.addressbook;
+/*
+ * Author : Rajesh
+ * Displaying welcome massage in the Address Book System.
+ */
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program!!");
+	}
+}
